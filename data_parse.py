50cf7aae81aac28bb8da2da6bf4c282e099b5bf4
jonahsimon36 <jonahsimon36@gmail.com>
2017-11-25 02:23:14 -0500

    added features for neural net

--- data_parse.py
+++ data_parse.py
@@ -2,6 +2,9 @@ import csv
 import datetime
 import dateutil.parser
 import numpy as np
+import matplotlib.pyplot
+
+
 
 def parse_csv(file):
     """
@@ -21,7 +24,7 @@ def parse_csv(file):
             except ValueError:
                 pass
     data_final = []
-    for row in data[1:]:
+    for row in data[1:50]:  ## developing with a subset for speed
         # parse the data
         date = dateutil.parser.parse(row[1]+" "+row[2])
         power_kw = row[3]
@@ -30,7 +33,51 @@ def parse_csv(file):
 
     return data_final
 
+def parse_holidays(file):
+    """
+    create array of holiday dates as data time objects from USBankHolidays.txt
+    :param data:
+    :return:
+    """
+    with open(file, newline='\n') as csv_file:
+        reader = csv.reader(csv_file)
+        holidays = []
+        for row in reader:
+            #holidays.append(dateutil.parser.parse(row[1]).date())
+            holidays.append(str(row[1]))
+        return holidays
+
+
+def generate_NN_features(data, holidays): # based off features used in Gajowniczek paper
+    """
+    generate features for the dataset
+    :param data:
+    :param holidays:
+    :return:
+    """
+
+    for i in range(len(data)):
+        hour = data[i][0].hour
+        for h in range(24):
+            data[i].append(hour == h)
+        wd = data[i][0].weekday()
+        for k in range(7):
+            data[i].append(wd == k)
+        md = data[i][0].day
+        for j in range(31):
+            data[i].append(md == j)
+        month = data[i][0].month
+        for l in range(12):
+            data[i].append(month == l)
+        print(holidays[20])
+        print(data[i][0].date())
+        data.append(data[i][0].date().isoformat() in holidays)  #I DO NOT KNOW WHY THIS IS NOT WORKING. THEY ARE BOTH STRINGS. ALSO TRIED DATETIME OBJECTS
+    return data
+
 
 if __name__ == '__main__':
     site1 = parse_csv("site_1.csv")
     print(site1[:10])
+    print("t:")
+    t = generate_NN_features(site1[:10], parse_holidays("USBankholidays.txt"))
+    print(t)
